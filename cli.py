79619d0c4f91f2b0dc5db0ba63a948cbb6f1dade
mabdulker <zjac264@live.rhul.ac.uk>
2022-06-17 17:52:25 +0100

    separated interface into categories, created menu page with multi-select prompt to allow user to navigate the application

--- cli.py
+++ cli.py
@@ -1,33 +1,57 @@
 
 
-# * PLAN
+# * Journey
 # 1. Authentication + API Session
 # 2. User inputs URL
 # 3. Run function x on Hugh's code 
 # 4. Refactor and add more documentation
 
 import fire
-from getpass import getpass
-import argparse
-
-def test():
-  return "Hello, this is a test!"
+import inquirer
+import os
+import re
 
 def bye(name="World"):
   return "Bye %s!" % name
+
+def displayMenu():
+  menu = [
+  inquirer.List('choice',
+                message="Menu",
+                choices=['Info', 'Login', 'Exit'],
+            ),
+  ]
+  return inquirer.prompt(menu)
+
+# TODO: implement regular expressions to validate input and add url query later
+def displayLogin():
+  query = [
+    inquirer.Text('username', message="Username"),
+    inquirer.Password('password', message="Password"),
+    # inquirer.Text('phone', message="What's your phone number",
+    #               validate=lambda _, x: re.match('\+?\d[\d ]+\d', x),
+    #             )
+  ]
+  answers = inquirer.prompt(query)
   
 
-if __name__ == '__main__':
-  username = input("Username:\n")
-  password = getpass()
-  url = input('What website do you want to check?\n')
+# TODO: create and style info page
+# TODO: add instantaneous quit option (e.g. press space to quit)
+def displayInfo():
+  print('lorem ipsum ...')
 
+
+if __name__ == '__main__':
   temp = True
   while temp:
-    inp = input()
-    if inp == 'q' or inp == 'exit':
-      temp = False
-    else :
-      print('I am here')
-      fire.Fire()
-
+    os.system('clear')
+    choice = displayMenu()['choice']
+    os.system('clear')
+    match choice:
+      case 'Info':
+        fire.Fire(displayInfo)
+      case 'Login':
+        fire.Fire(displayLogin)
+      case 'Exit':
+        temp = False
+    os.system('clear')
