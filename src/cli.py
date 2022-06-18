f9a6f6c93fde01e7910895a4d1e714052966b19c
mabdulker <zjac264@live.rhul.ac.uk>
2022-06-18 14:30:18 +0100

    Added markdown implementation for the text on the info page

--- src/cli.py
+++ src/cli.py
@@ -20,19 +20,23 @@ from rich import print
 from rich.console import Console
 from rich.text import Text
 from rich.traceback import install
+from rich.markdown import Markdown
 
-install()
-console = Console()
+install() #implements custom traceback styling || Rich
+console = Console() #creates new rich console || Rich
 
-""" Custom theme for the inquirer
-
+# TODO: fix below path not being universalq
 # Provides styling options for the inquirer list menu
-"""
-# TODO: fix below path not being universal
-f = open('src/inqTheme.json')
-data = json.load(f)
+script_dir = os.path.dirname(__file__)
+file_path1 = os.path.join(script_dir, 'inqTheme.json')
+f = open(file_path1)
+theme_data = json.load(f)
+customInq = inquirer.themes.load_theme_from_dict(theme_data)
+
+file_path2 = os.path.join(script_dir, 'info.md')
+f = open(file_path2, 'r')
+md = Markdown(f.read())
 f.close()
-x = inquirer.themes.load_theme_from_dict(data)
 
 """ Displays main menu
 
@@ -45,7 +49,7 @@ def displayMenu():
                 choices=['RELOAD_FOR_DEBUG', 'Info', 'Login', 'Exit'],
             ),
   ]
-  return inquirer.prompt(menu, theme=x)
+  return inquirer.prompt(menu, theme=customInq)
 
 # TODO: implement validation for customerID and password
 def displayLogin():
@@ -69,13 +73,13 @@ def validate_url(answer, current):
 # TODO: create and style info page
 def displayInfo():
   console.print(figlet_format('fair data', font = 'isometric3'), style="bold green")
-  # print('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ipsum orci, consectetur quis feugiat a, venenatis in nibh. Phasellus dignissim varius libero vitae pellentesque. Curabitur molestie erat magna, eu volutpat mi efficitur in. Curabitur vitae nisl at dolor congue elementum quis ac elit. Fusce at lorem ut mauris efficitur eleifend id eget lorem. Pellentesque sapien libero, cursus sed ipsum a, interdum pretium risus. Phasellus ornare ultricies elementum. Integer venenatis eleifend urna eu accumsan. Praesent tempor dui non eros consectetur facilisis. Fusce volutpat congue elit eu elementum. Nullam dapibus velit quam, in aliquet mi volutpat at. Nulla eget varius ex. Curabitur nec diam maximus, hendrerit nulla non, viverra felis. Nullam facilisis eget augue id posuere. Nam finibus erat vitae sapien sodales, fringilla tempus odio dictum. Ut quis dolor malesuada, consectetur ipsum at, auctor nulla.Donec eget accumsan neque, et varius mauris. Suspendisse eget felis ut risus rhoncus egestas vitae et metus. Nullam venenatis nisi augue, eu volutpat diam semper sed. Suspendisse potenti. Pellentesque congue lobortis ligula vel posuere. Aenean feugiat eros at interdum aliquam. Duis mattis felis quis purus finibus aliquam. Proin egestas nunc vel nulla ornare mollis. Donec fermentum massa ut dignissim feugiat. Curabitur aliquam dictum ex ut feugiat. Nulla aliquam convallis porttitor. Ut vehicula felis eu mauris viverra vestibulum. Nullam luctus scelerisque placerat. In eu nisi sit amet odio varius rutrum eu tincidunt nisl.Nulla non efficitur ligula, a hendrerit sem. Proin ipsum eros, vulputate id lorem non, dapibus cursus enim. Quisque suscipit ut dolor ut euismod. Nulla tristique, elit in aliquet sagittis, urna orci lobortis justo, ut lacinia leo felis sit amet augue. Donec pulvinar a erat quis volutpat. Duis vestibulum sollicitudin lacus ut egestas. Nam eu semper neque, nec malesuada lacus. Nullam vel leo congue neque ornare iaculis at ut ipsum. Curabitur sit amet enim lobortis, ornare lorem sed, pulvinar nisi. Suspendisse vel vulputate urna. Aliquam vulputate nisi magna, a congue felis gravida in. Praesent consectetur metus eu dolor vulputate, quis malesuada neque sodales. Suspendisse quis dapibus leo.Praesent laoreet, neque sit amet euismod laoreet, ex risus lacinia massa, et venenatis ipsum dolor eget elit. Fusce vitae est eu ligula pellentesque lobortis. Sed et imperdiet massa, sed efficitur nibh. Nulla semper vulputate felis sit amet congue. In vulputate mi dolor, consequat congue ipsum tristique non. Aenean luctus finibus nulla. Sed sit amet vulputate mauris. Nam et justo et dolor tempus ultricies. Nam tempor, ante sed euismod efficitur, massa lectus feugiat orci, vitae consectetur nulla neque quis ex. Etiam dignissim semper arcu, et faucibus nulla ullamcorper vitae. In vulputate est nec mi vulputate sollicitudin. Maecenas luctus, massa eu sollicitudin auctor, nisl mi malesuada velit, et faucibus ex tortor non orci. Duis rutrum est quis ipsum rutrum, eu tincidunt augue pulvinar.Praesent pretium in felis et bibendum. Donec varius vulputate nisl sed tincidunt. Praesent facilisis blandit ex, quis pellentesque mi ullamcorper quis. Integer placerat elit orci, sed facilisis justo pharetra nec. Mauris tempus nisl sed urna rutrum, et commodo ex tempor. Curabitur ac tincidunt lorem. Cras mattis eleifend tortor vel mollis. Sed dolor lorem, congue eget laoreet id, posuere quis lectus.\n')
-  print('hllo'.center(50))
+  console.print(md)
+  
+  # Exit info page
   print('\nPress Q to return back to the Menu: ')
   key = ''
   while key != 'q':
     key = getkey().lower()
-
 
 
 if __name__ == '__main__':
---
+++ src/info.md
@@ -0,0 +1,7 @@
+# Hello there
+
+Welcome to our ***fair*** data ...
+
+
+> Welcome
+>> bet you did not see this coming
