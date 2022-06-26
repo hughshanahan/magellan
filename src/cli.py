2b800cfc6b1b5e3a0e30a806e5e0748bf33fb00d
mabdulker <zjac264@live.rhul.ac.uk>
2022-06-26 16:50:17 +0300

    Separated URL inquiry field from credentials check, enabled user to maintain session_credentials via global boolean

--- src/cli.py
+++ src/cli.py
@@ -10,14 +10,11 @@ import os
 import re
 from click import style
 import sys
-import ssl
 
 #? Dependencies that need to be installed using pip install
 import inquirer #! (v2.8.0, as of yet no fix for backspace input error)
 import iso3166
 from getkey import getkey
-from numpy import void
-import requests 
 from yaspin import yaspin 
 from pyfiglet import figlet_format
 from rich import print
@@ -26,11 +23,13 @@ from rich.text import Text
 from rich.traceback import install
 from rich.markdown import Markdown
 
+# * Credentials for BrightData proxy service
 # lum-customer-c_0abac4c6-zone-unblocker
 # u6lk0tgc5mfo
 
 install() # implements custom traceback styling || rich.traceback
 console = Console() # creates new rich console || rich.console
+isLogedIn = False
 
 # * Provides styling options for the inquirer list menu
 script_dir = os.path.dirname(__file__)
@@ -47,11 +46,11 @@ f.close()
 
 
 
+""" 
+[D] DESCRIPTION | requests the url and returns the response
+[I] INPUT       | { url: string containing the url }
+"""
 def requestURL(url):
-  """ 
-  [D] DESCRIPTION | requests the url and returns the response
-  """
-
   countries = []
   for c in iso3166.countries_by_alpha2.keys():
     countries.append(c.lower())
@@ -67,57 +66,72 @@ def requestURL(url):
 
 
 
-def displayMenu():
-  """ 
-  [D] DESCRIPTION | displays main menu 
-  [R] RETURNS     | user's choice
-  [T] RETURN TYPE | dictionalry (e.g. "choice" : "Info")
-  """
-
+""" 
+[D] DESCRIPTION | displays main menu 
+[I] INPUT       | { choices: list of choices to display }
+[R] RETURNS     | user's choice
+[T] RETURN TYPE | dictionalry (e.g. "choice" : "Info")
+"""
+def displayMenu(choices):
   menu = [
   inquirer.List('choice',
                 message="MENU",
-                choices=['RELOAD_FOR_DEBUG', 'Info', 'Login', 'Exit'],
+                choices=choices,
             ),
   ]
   return inquirer.prompt(menu, theme=customInq)
 
 
 
-# TODO: implement validation for customerID and password
+""" 
+[D] DESCRIPTION | displays the request prompt to the user
+[R] RETURNS     | user's answers to the url query
+[T] RETURN TYPE | dictionary (e.g. "url" : "https://www.google.com")
+"""
+def displayRequest():
+  questions = [
+    inquirer.Text('url', message="URL",validate=validate_url),
+  ]
+  answer = inquirer.prompt(questions, theme=customInq)  
+
+  requestURL(answer['url'])
+
+
+
+""" 
+[D] DESCRIPTION | displays login promt to the user
+[R] RETURNS     | user's answers to the 3 queries
+[T] RETURN TYPE | dictionary (e.g. "url" : "https://www.google.com")
+"""
 def displayLogin():
-  """ 
-  [D] DESCRIPTION | displays login promt to the user
-  [R] RETURNS     | user's answers to the 3 queries
-  [T] RETURN TYPE | dictionary (e.g. "url" : "https://www.google.com")
-  """
+  global isLogedIn
 
   questions = [
     inquirer.Text('accountID', message="CustomerID"),
     inquirer.Password('password', message="Password"),
-    # inquirer.Text('url', message="URL",validate=validate_url),
   ]
   answer = inquirer.prompt(questions, theme=customInq)  
 
   if validate_user(answer['accountID'], answer['password']):
-    print('hello')
-    global CID, PSWRD
-    CID = answer['accountID']
-    PSWRD = answer['password']
+    print('Logging In...')
+    isLogedIn = True
+    # global CID, PSWRD
+    # CID = answer['accountID']
+    # PSWRD = answer['password']
   else: 
-    print('bye')
-  input()
+    print('Failed to Log in')
+  input() # ! pause for user to read
 
-  # requestURL(answer['url'])
 
+
+""" 
+[D] DESCRIPTION | validates the user's credentials
+[I] INPUT       | { customerID: string containing user's customerID }
+                  { password: string containing user's password }
+[R] RETURNS     | True if user's credentials are valid, else False
+[T] RETURN TYPE | boolean
+"""
 def validate_user(customerID, password):
-  """ 
-  [D] DESCRIPTION | validates the user's credentials
-  [I] INPUT       | { customerID: string containing user's customerID }
-                    { password: string containing user's password }
-  [R] RETURNS     | True if user's credentials are valid, else False
-  [T] RETURN TYPE | boolean
-  """
   # * Auth for BrightData proxy service 
   if sys.version_info[0]==3:
     import urllib.request
@@ -138,27 +152,26 @@ def validate_user(customerID, password):
     try:
       opener.open('http://youtube.com').read()
     except urllib.request.HTTPError as e:
-      match e.code:
-        case 401:
-          print('blacklisted_ip')
-        case 407:
-          print('wrong_credentials')
-      print(e)
+      # match e.code:
+      #   case 401:
+      #     print('blacklisted_ip')
+      #   case 407:
+      #     print('wrong_credentials')
+      # print(e)
       return False
 
     return True
 
 
 
+""" 
+[D] DESCRIPTION | validates the url
+[I] INPUT       | { answer: dictionary containing pervious user input } 
+                  { current: string that represents user input i.e. url }
+[R] RETURNS     | True if input is valid, else raises exception
+[T] RETURN TYPE | boolean / exception
+"""
 def validate_url(answer, current):
-  """ 
-  [D] DESCRIPTION | validates the url
-  [I] INPUT       | { answer: dictionary containing pervious user input } 
-                    { current: string that represents user input i.e. url }
-  [R] RETURNS     | True if input is valid, else raises exception
-  [T] RETURN TYPE | boolean / exception
-  """
-
   regex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
@@ -175,22 +188,20 @@ def validate_url(answer, current):
 # - myConfig known components: zone, customer, password
 
 # TODO: create and style info page
+""" 
+[D] DESCRIPTION | displays the markdown file with all of the essential information
+"""
 def displayInfo():
-  """ 
-  [D] DESCRIPTION | displays the markdown file with all of the essential information
-  """
-
   console.print(figlet_format('fair data', font = 'isometric3'), style="bold yellow")
   console.print(md)
   exitPage()
   
 
 
+""" 
+[D] DESCRIPTION | a widget to allow user to exit a page
+"""
 def exitPage():
-  """ 
-  [D] DESCRIPTION | a widget to allow user to exit a page
-  """
-
   qText = Text('\n\nPress Q to return back to the Menu: ')
   qText.stylize("bold green", 0, 9)
   console.print(qText)
@@ -201,9 +212,14 @@ def exitPage():
 
 
 
+"""
+[D] DESCRIPTION | main function
+"""
 if __name__ == '__main__':
   temp = True
   reload = False
+  loggedIn = ['RELOAD_FOR_DEBUG', 'Info', 'Run', 'Logout', 'Exit']
+  loggedOut = ['RELOAD_FOR_DEBUG', 'Info', 'Login', 'Exit']
 
   # os.system('clear')
   # displayInfo()
@@ -211,7 +227,8 @@ if __name__ == '__main__':
 
   while temp:
     os.system('clear')
-    choice = displayMenu()['choice']
+    x = loggedIn if isLogedIn else loggedOut
+    choice = displayMenu(x)['choice']
     os.system('clear')
     
     match choice:
@@ -219,7 +236,10 @@ if __name__ == '__main__':
         displayInfo()
       case 'Login':
         displayLogin()
-        
+      case 'Logout':
+        isLogedIn = False
+      case 'Run':
+        displayRequest()
       case 'Exit':
         temp = False
       case 'RELOAD_FOR_DEBUG':
